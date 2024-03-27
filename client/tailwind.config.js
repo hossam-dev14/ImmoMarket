d91dae90008d8035334dada792584c68e5708c2e
hossamdev <hossamdev14@gmail.com>
2024-03-27 13:43:27 +0000

    Installing TailwindCSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+// import { colors } from "tailwindcss/colors"; 
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // primary: colors.blue,
+        'secondary': '#064862',
+        'white': '#ffffff',
+        'black': '#333333'
+      }
+    },
+  },
+  plugins: [],
+}
