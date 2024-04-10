231b9c440b8c8ffca9abaab24eb21606d8f789a7
hossamdev <hossamdev14@gmail.com>
2024-04-10 22:16:08 +0000

    Updated

--- api/main.js
+++ api/main.js
@@ -1,51 +1,70 @@
 import express from "express";
-import morgan from "morgan";
-import connectMongoDB from "./config/database.js";
-import authRouter from "./routes/auth.route.js";
-
 import dotenv from 'dotenv';
-import errorHandler from "./middleware/errorHandler.js";
-
-// Loading environment variables from .env file
-dotenv.config();
+import morgan from "morgan";
+import cookieParser from 'cookie-parser';
+import connectMongoDB from "./config/database.js";
+import cors from "cors";
+import errorMiddlewar from "./middleware/error.middleware.js";
+//  Routes
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import propertyRoute from './routes/property.route.js';
 
 // Initialize the express app
 const app = express();
 
-// Set the port of our application
-const port = process.env.PORT || 8080;
+// Loading environment variables from .env file
+dotenv.config();
 
 // Connect to mongodb
 connectMongoDB();
 
+// Set the port of our application
+const port = process.env.PORT || 8080;
+
+const corsOptions = {origin: "http://localhost:3000"};
+app.use(cors(corsOptions));
+
 // Middleware for parsing incoming requests data
 app.use(express.json());
+app.use(cookieParser());
 
 // Log requests to the console
 app.use(morgan('tiny'));
 
-// Use the '/api/auth' endpoint for the authRouter
-app.use('/api/auth', authRouter);
 
-// Middleware form Handling Error
-app.use('/api/auth', errorHandler);
+// Define routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use('/api/properties', propertyRoute)
 
-// Define Property schema
-// const PropertySchema = new mongoose.Schema({
-//   address: {
-//     type: String,
-//     required: true
-//   },
-//   price: {
-//     type: Number,
-//     required: true
-//   },
-//   // Add more properties like type (apartment, house), description, etc.
-// }); const Property = mongoose.model('Property', PropertySchema);
+
+
+
+
+// ----_____-----------___ FOR TESTING PURPOSE ___---------_______---------____// 
+import User from './models/user.model.js';
+// For testIGN---------------------- //
+import {authMiddleware} from './middleware/auth.middleware.js';
+
+
+/* TESTIIIING -------------------*/ 
+// Route to get the currently logged-in user  
+app.get('/me', authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select('-password');
+    console.log(user );
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 
 // API endpoint to get all properties
-// app.get('/api/user', async (req, res) => {
+// app.get('/api/property/all', async (req, res) => {
 //   try {
 //     const users = await User.find();
 //     res.json(users);
@@ -54,6 +73,16 @@ app.use('/api/auth', errorHandler);
 //     res.status(500).json({ message: 'Server Error' });
 //   }
 // });
+
+// ----_____-----------___ FOR TESTING PURPOSE ___---------_______---------____// 
+
+
+
+
+// Error middleware
+app.use('/api', errorMiddlewar); 
+// must be placed at the end, if not it will catch all routes and always return the error
+
 
 // Start the server.
 app.listen(port, function(err) {
@@ -65,4 +94,5 @@ app.listen(port, function(err) {
     console.log(`Server is Running on ${"http://localhost:" + port}`);
   }
 })
+
 
