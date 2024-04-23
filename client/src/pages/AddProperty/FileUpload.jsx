203e5069e7b799b5805f6e24ceddc9a05c18b475
hossamdev <hossamdev14@gmail.com>
2024-04-23 16:54:23 +0100

    Added images upload to add new property

---
+++ client/src/pages/AddProperty/FileUpload.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const FileUpload = () => {
+  const [file, setFile] = useState(null);
+
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('image', file);
+
+    try {
+      const res = await axios.post('http://localhost:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="file" onChange={onChange} />
+      <button type="submit">Upload</button>
+    </form>
+  );
+};
+
+export default FileUpload;
