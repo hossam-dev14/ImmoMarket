78b90a2a43d80895c963d9c66e9aa7180a03da33
hossamdev <hossamdev14@gmail.com>
2024-04-30 13:55:56 +0100

    updated and added comments

--- api/helpers/createError.js
+++ api/helpers/createError.js
@@ -1,4 +1,4 @@
-
+// Create a new Error object with a custom message and status code
 export default (statusCode, message) => {
   const error = new Error(message);
   error.statusCode = statusCode;
