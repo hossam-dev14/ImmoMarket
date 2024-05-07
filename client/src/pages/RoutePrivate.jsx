674445ec6fb190fafa8c6c28fa652d599b1e2a20
hossamdev <hossamdev14@gmail.com>
2024-05-07 15:48:47 +0100

    Added comment

--- client/src/pages/RoutePrivate.jsx
+++ client/src/pages/RoutePrivate.jsx
@@ -6,3 +6,5 @@ export default function RoutePrivate () {
   const {userInfo} = useSelector((state) => state.auth);
   return userInfo?.data ? <Outlet /> : <Navigate to="/signin" replace />;
 }
+
+// <Outlet /> : displays the page content that the user has access to.
