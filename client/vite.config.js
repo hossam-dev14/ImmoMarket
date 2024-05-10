3fbfa7bb668499d2b6baf4aff29cfc381f9d0eea
hossamdev <hossamdev14@gmail.com>
2024-05-10 20:01:42 +0100

    Updated proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://immomarket.onrender.com/',
         changeOrigin: true,
       },
     },
