b31297548d6aeef2cdfb07f7a58ac45ebb451f0e
hossamdev <hossamdev14@gmail.com>
2024-05-10 18:58:50 +0100

    Updated header logo

--- client/src/Layout/Header/index.jsx
+++ client/src/Layout/Header/index.jsx
@@ -1,50 +1,15 @@
 // import React from 'react';
 import logo from '/src/assets/images/logo.png'
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import {useSignOutUserMutation} from '../../store/user/apiSlice';
-import { signOut } from '../../store/user/authSlice';
-import Search from '../../components/Search';
-import SidebarToggle from '../../components/SidebarToggle';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   const {userInfo} = useSelector((state) => state.auth);
-  const [signOutUser, { isError }] = useSignOutUserMutation();
-  
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  
-  const handleSignOut = async () => {
-    try {
-      await signOutUser().unwrap();
-      dispatch(signOut());
-
-      navigate('/signin');
-
-      toast.success("Sign out successfully");
-    } catch (error) {
-      if (!isError) return;
-      toast.error("Failed to sign out: ", error.message);
-    }
-  };
-
-  
+    
   return (
     <header className="text-gray-600 body-font ease-in duration-300 sticky top-0 right-0 left-0 z-10">
       <nav className='bg-slate-200 shadow-md'>
         <div className='max-w-7xl mx-auto px-4 lg:px-10 flex justify-between py-3 items-center h-20'>
-          <Link to='/' className="flex h-14 title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <div className='w-14 pr-2'>
-              <img className='rounded-md w-full' 
-                src={logo} alt='immomarket' 
-              />
-            </div>
-            <span className="text-secondary text-2xl hidden md:block">ImmoMarket</span>
-          </Link>
-        {userInfo?.data ? (
-          <div />
-          ) : (
             <Link to='/' className="flex h-14 title-font font-medium items-center md:justify-start justify-center text-gray-900">
               <div className='w-14 pr-2'>
                 <img className='rounded-md w-full' 
@@ -53,14 +18,7 @@ export default function Header() {
               </div>
               <span className="text-secondary text-2xl hidden md:block">ImmoMarket</span>
             </Link>
-          )}
-
-          
-
-          {/* Serch bar */}
-          {/* <div className='min-w-[200px] hidden md:block'>
-            <Search />
-          </div> */}
+          <div />
 
           <div className='flex justify-between items-center text-md font-medium text-gray-600 '>
             <div className='flex items-center py-2'>
