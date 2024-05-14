d9954c0a548defd62410fcb999e64b6def644371
hossamdev <hossamdev14@gmail.com>
2024-05-14 02:31:47 +0100

    Deleted proxy /api

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,34 @@
-// import dotenv from 'dotenv';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // run package config
-// dotenv.config();
+dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // define process env
-  // define: {
-  //   'process.env': process.env
-  // },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        // target: 'https://immo-market-api.vercel.app/',
-        target: 'https://immo-market-api.vercel.app/',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+
+    // define process env
+    define: {
+      'process.env': { 
+        ...import.meta.env, 
+        NODE_ENV: JSON.stringify(mode) 
       },
     },
-  },
+    build: {
+      minify: isProduction,
+      terserOptions: {
+        compress: {
+          drop_console: isProduction,
+        },
+      },
+    },
+  };
 });
